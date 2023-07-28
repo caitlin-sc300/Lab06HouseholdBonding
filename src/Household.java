f2e13dc4e674cd3d66c82969f19a1a2a75fd077d
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-07-28 14:12:18 -0600

    Create Household.java

---
+++ src/Household.java
@@ -0,0 +1,15 @@
+public class Household {
+    //PASTE YOUR WORK FROM LAB05 HERE
+
+    /**
+     * Every household can enforce the bedtime as its child.
+     * For this method, check if it is past the household's instance of child's bedtime by comparing it to the time variable
+     * If it's past the child's bedtime, return "go to bed!"
+     * Otherwise, return "I guess you can watch a little more tv"
+     *
+     * @return string depending on if it's past the child's bedtime
+     */
+    public String enforceBedtime() {
+        return "";
+    }
+}
