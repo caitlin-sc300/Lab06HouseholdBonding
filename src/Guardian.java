e80a8191dd1a07674f9333a86c86efeb7ecc1aa8
Emilie Beck <98420041+themilkprince@users.noreply.github.com>
2023-07-28 14:12:02 -0600

    Create Guardian.java

---
+++ src/Guardian.java
@@ -0,0 +1,32 @@
+public class Guardian {
+    //PASTE YOUR WORK FROM LAB05 HERE
+
+    /**
+     * For this method, the guardian will take an instance of the child and determine how to play
+     * with them depending on their traits as follows:
+     * if the child's favorite color is blue, orange, or black, return "played pretend"
+     * if the child's sport includes the word "ball", return "played sports"
+     * if the child is older than 12 (not inclusive), return "played video games"
+     * if none of the above conditions are met, return "played board games"
+     *
+     * @param child
+     * @return appropriate string for what guardian and child did
+     */
+    public String play(Child child) {
+        return "";
+    }
+
+    /**
+     * Our guardian likes to watch tv. However, they are only able to watch TV if their child is asleep.
+     * For this method, compare the current time to the child's bedtime.
+     * If it's past the child's bedtime, return true
+     * Otherwise, return false
+     *
+     * @param child
+     * @param time
+     * @return true/false
+     */
+    public boolean watchTv(Child child, double time){
+            return true;
+    }
+}
