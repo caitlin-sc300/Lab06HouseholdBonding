a14d6a719f147fd93d62f917d51a79c74523da9d
caitlin-sc300 <caitlin.purrington@gmail.com>
2024-10-01 21:09:49 -0600

    Update Child.java

--- src/Child.java
+++ src/Child.java
@@ -1,5 +1,50 @@
 public class Child {
-    //PASTE YOUR WORK FROM LAB05 HERE
+    private String name;
+    private int age;
+    private double bedtime;
+    private String sport;
+    private String favColor;
+
+    public Child(String name, int age, double bedtime, String sport, String favColor){
+        this.name = name;
+        this.age = age;
+        this.bedtime = bedtime;
+        this.sport = sport;
+        this.favColor = favColor;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setBedtime(double bedtime){
+        this.bedtime = bedtime;
+    }
+    public double getBedtime() {
+        return bedtime;
+    }
+    public void setSport(String sport){
+        this.sport = sport;
+    }
+    public String getSport(){
+        
+        return sport;
+    }
+    public void setFavColor(String favColor){
+        this.favColor = favColor;
+    }
+    public String getFavColor(){
+        
+        return favColor;
+    }
+   
 
     /**
      * For this method, our child will take an instance of a dog to play fetch with.
@@ -12,12 +57,32 @@ public class Child {
      * @return string with appropriate phrase
      */
     public String playFetch(Dog dog) {
-        return "";
+        int dogAge = dog.getAge();
+
+        if (dogAge < 2 || dogAge > 12) {
+            return "didn't fetch the ball";
+        } else if (dogAge >= 2 && dogAge <= 5){
+            return "got the ball but didn't bring it back";
+        } else{
+            return "fetched the ball";;
     }
 
     public static void main(String[] args) {
         Child roman = new Child("Roman", 8, 9.30, "baseball", "yellow");
         Dog buster = new Dog("Buster", 4, "Golden Retriever");
         System.out.println(roman.playFetch(buster)); //should print "got the ball but didn't bring it back"
+    }
+Class Dog {
+    private String name;
+    private int age;
+    private String breed;
+
+    public Dog(String name, int age, String breed){
+        this.name = name;
+        this.age = age:
+        this.breed = breed;
+    }
+    public int get.age(){
+        return age;
     }
 }
